55c6a8c6736a262fe901308122a86d7c00c7a46e
Chaussaroux <alexchaussaroux@gmail.com>
2024-07-12 21:11:21 +0200

    Déploiement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import "@/app/ui/global.css";
+import { interFont as inter } from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // Fallback to an empty string if inter.className is undefined
+  const fontClassName = inter?.className || "";
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${fontClassName} antialiased`}>{children}</body>
     </html>
   );
 }
